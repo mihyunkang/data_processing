7a55fdb383e29eb27713b773f1acf47fc8b668e3
mihyunkang <algus737@gmail.com>
2020-01-31 13:47:04 +0900

    add generate_gaussian_noise

--- CycleGAN_data/generate_gaussian_noise.py
+++ CycleGAN_data/generate_gaussian_noise.py
@@ -1,6 +1,19 @@
 import numpy as np
+from skimage.external.tifffile import imsave, imread
 
 def make_gaussian_noise(height, width, std):
-    img_noisy = np.zeros((h,w), dtype=np.float32)
+    noise_img = np.zeros((height,width), dtype=np.float32)
     for h in range(height):
-        
+        for w in range(width):
+            # avg : 0, std : 1
+            std_norm = np.random.normal() 
+            # avg : 0, std : std
+            random_noise = std*std_norm
+            noise_img[h][w] = random_noise 
+    return noise_img
+
+def save_tiff(path, np_image):
+    imsave(path, np_image)
+    print("{} save".format(path))
+
+save_tiff("./1.tiff", make_gaussian_noise(512, 512, 1))
